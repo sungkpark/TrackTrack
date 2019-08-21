7663b3cf7cc31a85d7ce253a6025ed11a2d0c844
Sung Park <s.park-4@student.tudelft.nl>
2019-08-21 09:38:00 +0900

    Version 2

--- js/background.js
+++ js/background.js
@@ -1,7 +1,7 @@
 var timeTo = "";
 var countdown;
 var reset = false;
-var timeSet;
+var timeSet = "";
 var timeLeft;
 var timeout;
 var timeUp = false;
@@ -9,30 +9,9 @@ var interval;
 var hours = 0;
 var minutes = 0;
 var seconds = 0;
-//
-// chrome.runtime.onInstalled.addListener(function() {
-//    chrome.contextMenus.create({
-//      "id": "sampleContextMenu",
-//      "title": "Sample Context Menu",
-//      "contexts": ["selection"]
-//    });
-//  });
-
-// chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
-//   if(response.cmd=="startTimer") {
-//     timeSet = response.timeSet;
-//     startTimer();
-//     sendResponse({
-//
-//     })
-//   } else if(response.cmd=="resetTimer") {
-//     resetTimer();
-//   }
-// });
 
 function startTimer() {
   chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
-    timeSet = "";
     timeSet = response.timeSet;
   });
 
@@ -41,22 +20,17 @@ function startTimer() {
     return;
   }
 
-  // alert("Time set to: " + document.getElementById("timeSet").value);
-  // timeSet = document.getElementById("timeSet").value
   var date = timeTo.concat(new Date().getMonth()+1, " ", new Date().getDate(), ", ", new Date().getFullYear(), " ", timeSet, ":00");
   countdown = new Date(date).getTime();
 
   var now = new Date().getTime();
   timeLeft = countdown - now;
   timeout = setTimeout(ring, timeLeft);
-  // document.getElementById("timer").innerHTML = "TIME IS TICKING";
 
   updateTime();
-  // interval = setInterval(updateTime, 1000);
 }
 
 function updateTimeLeft() {
-  // var x = setInterval(function() {
   var now = new Date().getTime();
   timeLeft = countdown - now;
 }
@@ -64,29 +38,11 @@ function updateTimeLeft() {
 function updateTime() {
   updateTimeLeft();
 
-  // Time calculations for days, hours, minutes and seconds
-  // var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
   hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24))/(1000 * 60 * 60));
   minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
   seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
   interval = setTimeout(updateTime, 1000);
-  // interval = setInterval(function() {
-  //   getTimeLeft();
-  //
-  //   // Time calculations for days, hours, minutes and seconds
-  //   // var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-  //   hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-  //   minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-  //   seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-  //
-  //   // Output the result in an element with id="demo"
-  //   // document.getElementById("hour").innerHTML = hours;
-  //   // document.getElementById("minute").innerHTML = minutes;
-  //   // document.getElementById("second").innerHTML = seconds;
-  // }, 1000)
-  //
-  // // }, 1000)
 }
 
 function getHour() {
@@ -109,32 +65,11 @@ function resetTimer() {
   reset = false;
 }
 
-// function notify() {
-//   var notif = new Notification("TrackTrack Timer", {
-//     type: "basic",
-//     icon: "/img/icon128.png",
-//     body: "Time's Up!"
-//   });
-//   chrome.notifications.create("timeNotif", notif);
-//   // notification.close.bind(notification);
-//
-//   timeUp = false;
-// }
-
 function getTimeUp() {
   return timeUp;
 }
 
 function ring() {
-  // if(reset) {
-  //   // document.getElementById("hour").innerHTML = 0;
-  //   // document.getElementById("minute").innerHTML = 0;
-  //   // document.getElementById("second").innerHTML = 0;
-  //   // document.getElementById("timeSet").innerHTML.value = "00:00";
-  //   // document.getElementById("timer").innerHTML = "RESET TIME";
-  //   reset = false;
-  // }
-  // else {
   timeUp = true;
   clearTimeout(interval);
 
@@ -144,10 +79,6 @@ function ring() {
     body: "Time's Up!"
   });
   chrome.notifications.create("timeNotif", notif);
-  // notification.close.bind(notification);
 
   timeUp = false;
-  // notify();
-    // chrome.notifications.create("timeOut", options, didCreateNotification);
-  // }
 }
--- js/content.js
+++ js/content.js
@@ -1,5 +1,4 @@
 var bgpage = chrome.extension.getBackgroundPage();
-// var state = "idle";
 var interval;
 var timeLeft = 0;
 var timeSet;
@@ -7,113 +6,52 @@ var reset = false;
 var refreshDisplayTimeout;
 var ticking = false;
 
-// var hour;
-// var minute;
-// var second;
-// var timeTo;
-// var bgpage;
-
 document.addEventListener('DOMContentLoaded', function() {
-  // chrome.runtime.sendMessage({
-  //   if (state == "idle") {
-  //
-  //   }
-  // })
   refreshDisplay();
   document.getElementById("startbutton").addEventListener("click", function() {
     if(!ticking) {
       ticking = true;
       reset = false;
-      sendTimeSet();
-      timeSet = document.getElementById("timeSet").value;
+      setTime();
       bgpage.startTimer();
     }
-    //
-    // document.getElementById("timer").innerHTML = "TIME IS TICKING";
-    //
-    // // setTimeout(function() {
-    // //
-    // // })
-    //
-    // interval = setInterval(function() {
-    //   console.log("background is running.");
-    //   document.getElementById("hour").innerHTML = bgpage.getHour();
-    //   document.getElementById("minute").innerHTML = bgpage.getMinute();
-    //   document.getElementById("second").innerHTML = bgpage.getSecond();
-    //   console.log(bgpage.timeUp);
-    //   if(bgpage.timeUp) {
-    //     document.getElementById("timer").innerHTML = "TIME UP";
-    //     clearInterval(interval);
-    //   }
-    // }, 1000)
   });
   document.getElementById("resetbutton").addEventListener("click", function() {
-    // clearInterval(interval);
-    ticking = false;
-    reset = true;
-    bgpage.resetTimer()
-    document.getElementById("hour").innerHTML = 0;
-    document.getElementById("minute").innerHTML = 0;
-    document.getElementById("second").innerHTML = 0;
-    document.getElementById("timeSet").innerHTML = "00:00";
-    document.getElementById("timer").innerHTML = "RESET TIME";
+    resetPage();
   });
 })
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   document.getElementById("startbutton").addEventListener("click", chrome.runtime.sendMessage({
-//     cmd: "startTimer",
-//     timeSet: document.getElementById("timeSet").value
-//   }));
-//   document.getElementById("resetbutton").addEventListener("click", chrome.runtime.sendMessage({
-//     cmd: "resetTimer"
-//   }));
-// })
-
 function refreshDisplay() {
-  if (reset) {
-    refreshDisplayTimeout = setTimeout(refreshDisplay, 1000);
-    return;
-  }
-  else if (!ticking) {
-    refreshDisplayTimeout = setTimeout(refreshDisplay, 1000);
-    return;
-  }
-  else if(timeLeft<=1000) {
-    document.getElementById("second").innerHTML = "0";
-    document.getElementById("timer").innerHTML = "TIME UP";
-    return;
-  }
-  else {
-    document.getElementById("timeSet").innerHTML = timeSet;
-  }
-  timeLeft = bgpage.timeLeft;
-  document.getElementById("timer").innerHTML = "TIME IS TICKING";
-  document.getElementById("hour").innerHTML = bgpage.getHour();
-  document.getElementById("minute").innerHTML = bgpage.getMinute();
-  document.getElementById("second").innerHTML = bgpage.getSecond();
+  if (ticking) {
+    timeLeft = bgpage.timeLeft;
+    document.getElementById("timer").innerHTML = "TIME IS TICKING";
+    document.getElementById("hour").innerHTML = bgpage.getHour();
+    document.getElementById("minute").innerHTML = bgpage.getMinute();
+    document.getElementById("second").innerHTML = bgpage.getSecond();
 
+    if (timeLeft<=1000) {
+      document.getElementById("second").innerHTML = "0";
+      document.getElementById("timer").innerHTML = "TIME UP";
+      ticking = false;
+    }
+  }
   refreshDisplayTimeout = setTimeout(refreshDisplay, 1000);
 }
 
-function sendTimeSet() {
-  // chrome.runtime.sendMessage('1'.concat(document.getElementById("timeSet").value));
+function setTime() {
+  timeSet = document.getElementById("timeSet").value;
   chrome.runtime.sendMessage({
-    // cmd: "startTimer",
     timeSet: document.getElementById("timeSet").value
   })
 }
-// //
-// function reset() {
-//   chrome.runtime.sendMessage({
-//     cmd: "resetTimer"
-//   });
-// }
-// function refreshTime() {
-  // timeTo = document.getElementById("timeSet").value;
-  // setInterval(function() {
-    // document.getElementById("hour").innerHTML = bgpage.getHour();
-    // document.getElementById("minute").innerHTML = bgpage.getMinute();
-    // document.getElementById("second").innerHTML = bgpage.getSecond();
-  // }, 1000);
-// }
+
+function resetPage() {
+  ticking = false;
+  reset = true;
+  bgpage.resetTimer()
+  document.getElementById("hour").innerHTML = 0;
+  document.getElementById("minute").innerHTML = 0;
+  document.getElementById("second").innerHTML = 0;
+  document.getElementById("timeSet").innerHTML = "00:00";
+  document.getElementById("timer").innerHTML = "RESET TIME";
+}
